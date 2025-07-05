f991fffdd33c7702d62732c0d6e0a77428ca94b6
ricardoingeaa <115902704+ricardoingeaa@users.noreply.github.com>
2025-07-05 13:55:33 -0600

    activity week one, add element to cart

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,25 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
-function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+async function addProductToCart(product) {
+  const listProducts = await getLocalStorage("so-cart");
+
+  let cart = [];
+
+  try {
+    cart = listProducts ? JSON.parse(listProducts) : [];
+  } catch (e) {
+    console.error("Error ain parse localStorage:", e);
+    cart = [];
+  }
+
+  console.log("current cart:", cart);
+
+  cart.push(product);
+
+  setLocalStorage("so-cart", JSON.stringify(cart));
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
