1237540c5d1e796f0c4a0cd56f7f77e6cf7f2595
ricardoingeaa <115902704+ricardoingeaa@users.noreply.github.com>
2025-07-12 17:53:53 -0600

    Elements are visible

--- src/js/product.js
+++ src/js/product.js
@@ -1,26 +1,11 @@
 import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
+// Configuración inicial
 const dataSource = new ProductData("tents");
+const productId = new URLSearchParams(window.location.search).get("product");
 
-// To add a product to the cart, we need to append products to the existing array of products.
-function addProductToCart(product) {
-  const cartItems = getLocalStorage("so-cart") || []; // get cart array of items from local storage if null set to empty array
-  /* eslint-disable no-console */
-  // for debugging purposes
-  console.log("Before:", cartItems);
-  cartItems.push(product);
-  console.log("After:", cartItems);
-  /* eslint-enable no-console */
-  setLocalStorage("so-cart", cartItems);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+// Crea y ejecuta el controlador de vista de producto
+const productDetails = new ProductDetails(productId, dataSource);
+productDetails.init();
