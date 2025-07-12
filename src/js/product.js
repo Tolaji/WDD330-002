c542095c613af6d4079e6517cf2934a8c1aa957c
precious-israel <carisabiodun@gmail.com>
2025-07-12 15:58:48 +0100

    Completed W01 Add item to cart broken

--- src/js/product.js
+++ src/js/product.js
@@ -6,12 +6,18 @@ const dataSource = new ProductData("tents");
 // To add a product to the cart, we need to append products to the existing array of products.
 function addProductToCart(product) {
   const cartItems = getLocalStorage("so-cart") || []; // get cart array of items from local storage if null set to empty array
-  /* eslint-disable no-console */
-  // for debugging purposes
-  console.log("Before:", cartItems);
-  cartItems.push(product);
-  console.log("After:", cartItems);
-  /* eslint-enable no-console */
+
+  // Find existing product
+  const existingItem = cartItems.find((item) => item.Id === product.Id);
+
+
+  if (existingItem) {
+    existingItem.quantity = (existingItem.quantity || 1) + 1;
+  } else {
+    product.quantity = 1;
+    cartItems.push(product);
+  }
+
   setLocalStorage("so-cart", cartItems);
 }
 // add to cart button event handler
