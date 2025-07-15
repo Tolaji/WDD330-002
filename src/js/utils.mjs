a72a6ac95fe7eebd3c26496c47597921325d41a5
Rakieft <kieftraphter.joly1@gmail.com>
2025-07-15 15:49:14 -0400

    another commit

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -27,3 +27,11 @@ export function getParam(parameter){
   const urlParams = new URLSearchParams(queryString);
   const product = urlParams.get(parameter)
 }
+
+export function renderListWithTemplate(template, parentElement, list, position = "afterbegin", clear = false) {
+  const htmlStrings = list.map(template);
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+}
