f1b3aa644db42c8fb03b0c71c0256cf7eac3e563
Etim 97 <Wisdomlds97@gmail.com>
2025-07-19 00:28:47 +0100

    wk2

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,5 +1,19 @@
-import { renderListWithTemplate } from "./utils.mjs";
+// Template function for a product card
+export function productCardTemplate(product) {
+  return `
+    <li class="product-card">
+      <a href="product_pages/index.html?product=${product.Id}">
+        <img src="${product.Image}" alt="${product.NameWithoutBrand}" />
+        <h2 class="card__brand">${product.Brand.Name}</h2>
+        <h3 class="card__name">${product.NameWithoutBrand}</h3>
+        <p class="product-card__price">$${product.FinalPrice}</p>
+      </a>
+    </li>
+  `;
+}
 
+import { renderListWithTemplate } from "./utils.mjs";
+import { productCardTemplate } from "./ProductList.mjs"; 
 export default class ProductList {
   constructor(category, dataSource, listElement) {
     this.category = category;
@@ -12,15 +26,27 @@ export default class ProductList {
     this.renderList(list);
   }
 
-  // renderList(list) {
-  // const htmlStrings = list.map(productCardTemplate);
-  // this.listElement.insertAdjacentHTML('afterbegin', htmlStrings.join(''));
-  // }
+  renderList(list, clear = false) {
+  // Clear the element if clear is true
+  if (clear) {
+    this.listElement.innerHTML = '';
+  }
+  // Use the imported productCardTemplate function for each product
+  const htmlStrings = list.map(productCardTemplate);
+  this.listElement.innerHTML += htmlStrings.join('');
+}
+  renderList(list) {
+    // Use the imported productCardTemplate function for each product
+    const htmlStrings = list.map(productCardTemplate);
+    this.listElement.innerHTML = htmlStrings.join('');
+  }
 
-  // renderList(list) {
-  //   const htmlList = list.map(product => this.productCardTemplate(product)).join("");
-  //   this.listElement.innerHTML = htmlList;
-  // }
+
+
+  renderList(list) {
+    const htmlList = list.map(product => this.productCardTemplate(product)).join("");
+    this.listElement.innerHTML = htmlList;
+  }
 
    renderList(list) {
     renderListWithTemplate(this.productCardTemplate, this.listElement, list, "afterbegin", true);
@@ -28,19 +54,19 @@ export default class ProductList {
 
   
 
-  // productCardTemplate(product) {
-  //   const template = `
-  //     <li class="product-card">
-  //       <a href="product_pages/index.html?product=${product.Id}">
-  //         <img src="${product.Image}" alt="${product.NameWithoutBrand}" />
-  //         <h2 class="card__brand">${product.Brand.Name}</h2>
-  //         <h3 class="card__name">${product.NameWithoutBrand}</h3>
-  //         <p class="product-card__price">$${product.FinalPrice}</p>
-  //       </a>
-  //     </li>
-  //   `;
-  //   return template;
-  // }
+  productCardTemplate(product) {
+    const template = `
+      <li class="product-card">
+        <a href="product_pages/index.html?product=${product.Id}">
+          <img src="${product.Image}" alt="${product.NameWithoutBrand}" />
+          <h2 class="card__brand">${product.Brand.Name}</h2>
+          <h3 class="card__name">${product.NameWithoutBrand}</h3>
+          <p class="product-card__price">$${product.FinalPrice}</p>
+        </a>
+      </li>
+    `;
+    return template;
+  }
 
   productCardTemplate(product) {
     return `
