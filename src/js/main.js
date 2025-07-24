72fcaab7652c1aeab969d92bf50fb42d0a26e1a8
Jojera <jojera@byupathway.edu>
2025-07-24 18:23:51 +0300

    customizable alert

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,13 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
-import { updateCartCount } from "./utils.mjs";  
+import { updateCartCount } from "./utils.mjs";
+import Alert from "./alert.js";
+
+// locates the root index.html file and renders the alert there
+if (window.location.pathname === "/index.html") {
+  const alert = new Alert("/json/alerts.json");
+  alert.render(document.querySelector("main"));
+}
 
 const category = "tents";
 const dataSource = new ProductData(category);
