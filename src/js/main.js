bce21f57961aa817a60c89896ed1d9f28cff8dac
Jojera <jojera@byupathway.edu>
2025-07-24 18:23:51 +0300

    customizable alert

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
-import { updateCartCount, getParam } from "./utils.mjs";
+import { updateCartCount } from "./utils.mjs";
 import Alert from "./alert.js";
 
 // locates the root index.html file and renders the alert there
@@ -8,6 +8,7 @@ if (window.location.pathname === "/index.html") {
   const alert = new Alert("/json/alerts.json");
   alert.render(document.querySelector("main"));
 }
+
 
 const category = getParam("product");
 const dataSource = new ProductData(category);
