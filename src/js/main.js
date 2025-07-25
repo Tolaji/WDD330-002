875de780271625a93bf8536922e75f14e1e7359c
ricardoingeaa <115902704+ricardoingeaa@users.noreply.github.com>
2025-07-25 07:31:48 -0600

    solve in main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,13 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 import { updateCartCount, getParam } from "./utils.mjs";
+import Alert from "./alert.js";
+
+// locates the root index.html file and renders the alert there
+if (window.location.pathname === "/index.html") {
+  const alert = new Alert("/json/alerts.json");
+  alert.render(document.querySelector("main"));
+}
 
 const category = getParam("product");
 const dataSource = new ProductData(category);
